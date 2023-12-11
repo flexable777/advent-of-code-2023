86b3c1c285936f32a222def163fff585b79d0a7a
Andreas Jonsson <andreas.jonsson@nav.no>
2023-12-11 22:07:22 +0100

    Prepare for day eleven.

---
+++ src/Day11.kt
@@ -0,0 +1,20 @@
+fun main() {
+    fun part1(input: List<String>): Long {
+
+        return 1L
+    }
+
+    fun part2(input: List<String>): Long {
+
+        return 1L
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInputAsStringLines("Day11_test")
+    check(part1(testInput) == 1L)
+    check(part2(testInput) == 1L)
+
+    val input = readInputAsStringLines("Day11")
+    part1(input).println(1)
+    part2(input).println(2)
+}
